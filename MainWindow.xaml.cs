aff24ea9e2c17edfdda253762eab145ddbdef73c
Михаил Серых <masterlevwin@mail.ru>
2024-05-29 17:16:48 +0300

    Упрощен метод CheckVersion()

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Metal_Code.Updater
 {
@@ -20,9 +10,31 @@ namespace Metal_Code.Updater
     /// </summary>
     public partial class MainWindow : Window
     {
+        string serverPath = $"Y:\\Конструкторский отдел\\Расчет Заказов ЛФ Сервер\\Metal-Code_Local\\Metal-Code_Local";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (CheckVersion()) UpdateApp();
+
+            Process.Start(Directory.GetCurrentDirectory() + "\\Metal-Code.exe");
+            Environment.Exit(0);
+        }
+
+        public bool CheckVersion()
+        {
+            FileInfo serverVersionFile = new FileInfo(serverPath + "\\version.txt");
+            FileInfo localVersionFile = new FileInfo(Directory.GetCurrentDirectory() + "\\version.txt");
+
+            return serverVersionFile.Exists && localVersionFile.Exists
+                && File.ReadAllText(serverPath + "\\version.txt") == File.ReadAllText(Directory.GetCurrentDirectory() + "\\version.txt");
+        }
+
+        public void UpdateApp()
+        {
+            FileInfo lib = new FileInfo(serverPath + "\\Metal-Code.dll");
+            lib.CopyTo(Directory.GetCurrentDirectory() + "\\Metal-Code.dll", true);
         }
     }
 }
